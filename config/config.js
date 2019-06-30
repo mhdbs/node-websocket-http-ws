fa9d45df2779e97c8d8ab180a7ba19787f7f3f63
Mohammed Bilal <mohammedbilal1995@gmail.com>
2019-06-30 21:59:20 +0530

    included docker files

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 
 require('dotenv').config();
-const REDIS_URL = process.env.REDIS_URL || "localhost"
+const REDIS_URL = process.env.REDIS_URL || "redis"
 const REDIS_PORT = process.env.REDIS_PORT || 6379
 const CLIENT = process.env.CLIENT_SOCKET || "http://localhost:3000"
 const DEVICE_ID = process.env.DEVICE_ID || "testdevice" 
